fc4dd77c5e57e7aef8925d8560e2db85d9a40437
Gumaan-Mahar <gumaanmahar@gmail.com>
2021-02-25 13:13:37 +0500

    fixes

--- lib/widgets/custom_toast.dart
+++ lib/widgets/custom_toast.dart
@@ -5,7 +5,7 @@ showToast({String text}) {
   Fluttertoast.showToast(
       msg: text,
       toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
+      gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 1,
       backgroundColor: Colors.black,
       textColor: Colors.white,
